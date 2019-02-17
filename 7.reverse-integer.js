0e0d314aadc01fc309989c2aabfd96bd660397bf
yeshu.lrt <yeshu.lrt@alibaba-inc.com>
2019-02-17 18:26:13 +0800

    :sparkles: solve 7.reverse-integer

---
+++ 7.reverse-integer.js
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode.cn id=7 lang=javascript
+ *
+ * [7] 整数反转
+ *
+ * https://leetcode-cn.com/problems/reverse-integer/description/
+ *
+ * algorithms
+ * Easy (31.09%)
+ * Total Accepted:    74.3K
+ * Total Submissions: 238.9K
+ * Testcase Example:  '123'
+ *
+ * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+ * 
+ * 示例 1:
+ * 
+ * 输入: 123
+ * 输出: 321
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: -123
+ * 输出: -321
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 输入: 120
+ * 输出: 21
+ * 
+ * 
+ * 注意:
+ * 
+ * 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+ * 
+ */
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse1 = function (x) {
+  let result = 0, max = Math.pow(2, 31) - 1, min = - Math.pow(2, 31);
+  const reverseStr = x => String(x).split('').reverse().join('');
+  if (x >= 0) {
+    result = reverseStr(x);
+  } else {
+    result = - reverseStr(Math.abs(x));
+  }
+  if (result > max || result < min) {
+    result = 0;
+  }
+  return result;
+};
+// 官方题解
+var reverse = function (x) {
+  let temp = x, rev = 0, max = Math.pow(2, 31) - 1, min = - Math.pow(2, 31);
+  while (parseInt(temp, 10) != 0) {
+    let pop = parseInt(temp % 10, 10);
+    temp /= 10;
+    if (rev > max / 10 || (rev == parseInt(max / 10, 10) && pop > 7)) return 0;
+    if (rev < min / 10 || (rev == parseInt(min / 10, 10) && pop < -8)) return 0;
+    rev = rev * 10 + pop;
+  }
+  return rev;
+};
